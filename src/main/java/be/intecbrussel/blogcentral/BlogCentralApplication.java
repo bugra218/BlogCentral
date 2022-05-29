31e9924ada46d2d2eddaaa9032180eec103bd74f
bugra <bugra.seyratli@gmail.com>
2022-05-29 12:33:20 +0200

    necessary library needs to be added

---
+++ src/main/java/be/intecbrussel/blogcentral/BlogCentralApplication.java
@@ -0,0 +1,13 @@
+package be.intecbrussel.blogcentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogCentralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogCentralApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/intecbrussel/blogcentral/BlogCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package be.intecbrussel.blogcentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCentralApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
