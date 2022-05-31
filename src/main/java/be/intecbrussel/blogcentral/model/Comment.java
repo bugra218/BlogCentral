ec81635e09ab478e6642587393e2c5b2a11c6f07
erikh <91531357+erikholl@users.noreply.github.com>
2022-05-31 12:34:25 +0200

    model drafted

---
+++ src/main/java/be/intecbrussel/blogcentral/model/Comment.java
@@ -0,0 +1,37 @@
+package be.intecbrussel.blogcentral.model;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.mapping.ToOne;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name="blog_post_id")
+    private BlogPost blogPost;
+
+    @ManyToOne
+    @JoinColumn(name="author_id")
+    private Author author;
+
+    @Column(name="body")
+    private String body;
+
+    @CreationTimestamp
+    @Column(name="timestamp_created")
+    private Date timestampCreated;
+
+}
