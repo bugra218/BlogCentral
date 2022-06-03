7ff1edf2b71fdcf9d0ab386b781ead6622bf7b51
erikh <91531357+erikholl@users.noreply.github.com>
2022-06-03 16:16:44 +0200

    AuthorController completion drafted

--- src/main/java/be/intecbrussel/blogcentral/controller/AuthorController.java
+++ src/main/java/be/intecbrussel/blogcentral/controller/AuthorController.java
@@ -5,11 +5,15 @@ import be.intecbrussel.blogcentral.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
+// TODO: test
+
 @Controller
+@RequestMapping("/authors")
 public class AuthorController {
     private AuthorService authorService;
 
@@ -18,10 +22,58 @@ public class AuthorController {
         this.authorService = authorService;
     }
 
-    @GetMapping("getAllAuthors")
+    // get all Authors
+    @GetMapping("/")
     public String getAllAuthors(Model model){
         List<Author> authorsFromDb = authorService.getAllAuthors();
         model.addAttribute("authors", authorsFromDb);
-        return "authors";
+        return "all-authors";
     }
+
+    // get register-form new Author
+    @GetMapping("/register")
+    public String registerAuthor() {
+        return "author-profile-form"; // placeholder
+    }
+
+    // save new Author
+    @PostMapping("/save")
+    public String saveAuthor(@ModelAttribute("author") Author author) {
+        authorService.createAuthor(author);
+        return "redirect:/"; // placeholder
+        // TODO: redirect to page where 'create Author' was initiated
+    }
+
+    // get an Author based on id - return Author home page
+    // TODO: consider to give username as parameter and use that as URL for
+    //  Author homepage
+    @GetMapping("/{id}")
+    public String showAuthorPage(@PathVariable String id, Model model) {
+        Integer idInt = Integer.parseInt(id);
+        Author author = authorService.getAuthorById(idInt);
+        model.addAttribute(author);
+        return "authorPage"; // placeholder
+    }
+
+    // update Author - get author based on id - return author profile form
+    // prefilled with author details - TODO: not sure if this way works, how
+    //  will the fields be pre-filled?
+    @GetMapping("/update/{id}")
+    public String showAuthorProfileForm(@PathVariable String id,
+                                              Model model) {
+        Integer idInt = Integer.parseInt(id);
+//        ModelAndView mav = new ModelAndView("author-profile-form");
+        Author author = authorService.getAuthorById(idInt);
+        model.addAttribute(author); // placeholder
+        return "author-profile-form";
+    }
+
+    // delete Author
+    @GetMapping("/delete/{id}")
+    public String deleteAuthor(@PathVariable String id) {
+        Integer idInt = Integer.parseInt(id);
+        this.authorService.deleteAuthorById(idInt);
+        return "redirect:/"; // placeholder
+    }
+
 }
