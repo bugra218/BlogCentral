6ed6032fbd045289dfe5013c2fc992a23cea8564
Loïc Deketelaere <zehsrafe@gmail.com>
2022-06-07 20:24:09 +0200

    added ability to like and unlike posts

--- src/main/java/be/intecbrussel/blogcentral/repository/LikeRepository.java
+++ src/main/java/be/intecbrussel/blogcentral/repository/LikeRepository.java
@@ -1,10 +1,13 @@
 package be.intecbrussel.blogcentral.repository;
 
+import be.intecbrussel.blogcentral.model.Author;
+import be.intecbrussel.blogcentral.model.BlogPost;
 import be.intecbrussel.blogcentral.model.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Integer> {
-    Like countByBlogPost_Id(int id);
+    int countLikeByBlogPost_Id(int id);
+    Like findLikeByBlogPostAndAuthor(BlogPost blogPost, Author author);
 }
