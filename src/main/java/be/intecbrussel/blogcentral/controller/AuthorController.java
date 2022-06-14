cb81d11625c2d2515e743e41e3bebbf076f642fd
bugra <bugra.seyratli@gmail.com>
2022-06-14 15:26:50 +0200

    new commit

--- src/main/java/be/intecbrussel/blogcentral/controller/AuthorController.java
+++ src/main/java/be/intecbrussel/blogcentral/controller/AuthorController.java
@@ -1,28 +1,28 @@
 package be.intecbrussel.blogcentral.controller;
 
-import be.intecbrussel.blogcentral.Exceptions.*;
+
 import be.intecbrussel.blogcentral.model.Author;
 import be.intecbrussel.blogcentral.model.BlogPost;
 import be.intecbrussel.blogcentral.service.AuthorService;
 import be.intecbrussel.blogcentral.service.BlogpostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
+import javax.validation.Valid;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Objects;
 
 // TODO: exception handling, avoid duplicate code with help methods
 @Controller
 @RequestMapping("/authors")
 public class AuthorController {
     private AuthorService authorService;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     private BlogpostService blogpostService;
     private String comingFrom = "";
 
@@ -46,71 +46,21 @@ public class AuthorController {
         return "all-authors"; // placeholder
     }
 
-    // get register-form new Author
     @GetMapping("/register")
-    public String registerAuthor(HttpServletRequest request) {
-        comingFrom = request.getHeader("Referer");
-
-        System.out.println(comingFrom);
-
+    public String registerAuthor(Model model) {
+        model.addAttribute("author", new Author());
         return "create-author"; // placeholder
     }
 
-    // save new Author
-    // BindingResult result in arguments is NEEDED
-    // TODO: update author profile makes use of this save as well - update
-    //  should however exclude the check on username exists (separate save
-    //  for update?)
     @PostMapping("/save")
-    public String saveAuthor(@ModelAttribute("author") Author author, BindingResult result, RedirectAttributes attributes, HttpServletRequest request) {
-        try {
-            // check if all required fields are filled
-            List<String> requiredFields = new ArrayList<>();
-            String authorEmail = author.getEmail();
-            requiredFields.add(author.getUserName());
-            requiredFields.add(author.getPassword());
-            requiredFields.add(author.getFirstName());
-            requiredFields.add(authorEmail);
-            if (requiredFields.contains("")) {
-                throw new RequiredFieldsException();
-            }
+    public String saveAuthor(@Valid @ModelAttribute("author") Author author, BindingResult result, Model model) {
 
-            // check if email format is correct
-            String[] splitedEmail;
-            if (authorEmail.contains("@")) {
-                splitedEmail = authorEmail.split("@");
-                if (!splitedEmail[1].contains(".")) {
-                    throw new EmailFormatException();
-                }
-            } else {
-                throw new EmailFormatException();
-            }
-
-            // check if zipcode is an actual number (value will default to 0 if a string was inputted instead)
-            if (Objects.equals(author.getZip(), 0)) {
-                throw new ZipcodeException();
-            }
-
-//             checks if inputted username is still available
-            if (authorService.usernameExists(author.getUserName())) {
-                throw new UsernameNotAvailableException();
-            }
-
-            authorService.createAuthor(author);
-
-            if (comingFrom != null) {
-                return "redirect:" + comingFrom;
-            } else return "redirect:"; // placeholder
-
-        } catch (RequiredFieldsException | UsernameNotAvailableException | ZipcodeException | EmailFormatException e) {
-            e.printStackTrace();
-            attributes.addFlashAttribute( "errorMessage", e.getMessage());
-            return "redirect:" + request.getHeader("Referer");
-        } catch (Exception e) {
-            e.printStackTrace();
-            attributes.addFlashAttribute( "errorMessage", "An unknown error occurred.");
-            return "redirect:" + request.getHeader("Referer");
+        if(result.hasErrors()) {
+            model.addAttribute("author", author);
+            return "create-author";
         }
+        authorService.createAuthor(author);
+        return "redirect:/"; // placeholder
     }
 
     // get an Author based on id - return Author home page
--- src/main/java/be/intecbrussel/blogcentral/model/Author.java
+++ src/main/java/be/intecbrussel/blogcentral/model/Author.java
@@ -3,6 +3,7 @@ package be.intecbrussel.blogcentral.model;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 @Entity
 @Getter
@@ -16,28 +17,48 @@ public class Author {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
-    @Column(name="first_name")
+    @Column(name = "first_name")
+    @NotBlank(message = "Please enter your first name using letters")
+    @Size(min = 3, max = 20, message = "Your First name can not be less than 3 character or greater than 20")
     private String firstName;
 
-    @Column(name="last_name")
+    @Column(name = "last_name")
+    @NotBlank(message = "Please enter your last name using letters")
+    @Size(min = 3, max = 20, message = "Your last name can not be less than 3 character or greater than 20")
     private String lastName;
 
-    @Column(name="user_name")
+    @Column(name = "user_name", unique = true)
+    @NotBlank(message = "Please enter your user name using letters")
+    @Size(min = 3, max = 20, message = "Your user name can not be less than 3 character or greater than 20")
     private String userName;
 
+    @NotBlank(message = "Please enter your password using letters")
     private String password;
 
+    @NotBlank(message = "Please enter your email using letters")
+    @Size(min = 3, max = 20, message = "Your email can not be less than 3 character or greater than 20")
+    @Email(message = "Invalid Email. please enter the correct email")
     private String email;
 
+
+    @NotBlank(message = "Please enter your street using letters")
+    @Size(min = 3, max = 20, message = "Your street can not be less than 3 character or greater than 20")
     private String street;
 
-    @Column(name="house_number")
+    @Column(name = "house_number")
+    @NotBlank(message = "Please enter your user name using letters")
+    @Size(min = 3, max = 20, message = "Your user name can not be less than 3 character or greater than 20")
     private String houseNr;
 
+    @NotBlank(message = "Please enter your user name using letters")
+    @Size(min = 3, max = 20, message = "Your user name can not be less than 3 character or greater than 20")
     private String city;
+
+    @Digits(integer = 9, fraction = 0, message = "zip code can not be more than 9 digit")
+    @Positive(message = "zip code can not be a negative number")
     private int zip;
 
-    @Column(name="avatar_path")
+    @Column(name = "avatar_path")
     private String avatarPath;
 
 //    @Column(name="blog_posts")
