fdb64d15f2fcc56d2d8897264df62485f301591e
Loïc Deketelaere <zehsrafe@gmail.com>
2022-06-17 22:40:01 +0200

    Final version

--- src/main/java/be/intecbrussel/blogcentral/controller/LikeController.java
+++ src/main/java/be/intecbrussel/blogcentral/controller/LikeController.java
@@ -11,9 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.Objects;
 
 @Controller
 public class LikeController {
